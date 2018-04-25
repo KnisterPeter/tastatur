17d0a539dd3652ee19a6e9ac6c8ccc30e4af5fba
Markus Wolf <markus.wolf@magicline.de>
2018-04-25 17:05:59 +0200

    test: add install/uninstall test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,46 @@
 // tslint:disable:no-unused-expression
 import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
 
-describe('just', () => {
-  it('in case', () => {
-    expect(true).to.be.true;
+import { install, bind, uninstall } from '../lib/index';
+
+describe('keyboard', () => {
+  describe('when installed', () => {
+    let dom!: JSDOM;
+    let document!: Document;
+
+    beforeEach(() => {
+      dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
+      document = dom.window.document;
+      install(document);
+    });
+
+    afterEach(() => {
+      uninstall(document);
+    });
+
+    it('should respond to bound keys', () => {
+      let reactedToKey = false;
+
+      bind('a', () => {
+        reactedToKey = true;
+      });
+      document.dispatchEvent(new dom.window.KeyboardEvent('keydown', {
+        code: 'KeyA'
+      }));
+
+      expect(reactedToKey).to.be.true;
+    });
+  });
+  describe('when uninstalled', () => {
+    it('should not respond to bound keys', () => {
+      let reactedToKey = false;
+      bind('a', e => {
+        console.log(e);
+        reactedToKey = true;
+      });
+
+      expect(reactedToKey).to.be.false;
+    });
   });
 });
