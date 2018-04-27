e6e6f54e6b137fa02388a1a18da5cf42c52eb0d9
Markus Wolf <markus.wolf@magicline.de>
2018-04-27 15:32:37 +0200

    Update page

--- index.js
+++ index.js
@@ -212,6 +212,7 @@ var Tastatur = /** @class */ (function () {
         else if (e.type === 'keyup') {
             this.pressed[this.mapKeyCode(e)] = false;
         }
+        console.log(this.registrations);
         console.log(this.pressed);
         var registration = this.registrations.find(function (registration) {
             var required = _this.areRequiredKeysPressed(registration);
