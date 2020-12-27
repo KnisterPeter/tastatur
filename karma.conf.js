5341855fb7dacb7aa636f95f686bb023aa1d8d7c
Markus Wolf <KnisterPeter@users.noreply.github.com>
2020-12-27 13:54:43 +0100

    chore: setup github actions (#610)
    * chore: setup github actions
    
    * chore: configure workflow
    
    * chore: update karma setup
    
    * chore: set env in test execution

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,38 @@
-const isTravis = !!process.env.TRAVIS_BUILD_NUMBER;
-
 const browsers = {
-  slSafari10: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.12',
-    version: '10.1'
-  },
   slSafari11: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.13',
-    version: '11'
+    base: "SauceLabs",
+    browserName: "safari",
   },
-  // slIE11: {
-  //   base: 'SauceLabs',
-  //   browserName: 'internet explorer',
-  //   platform: 'Windows 7',
-  //   version: '11'
-  // },
-  // slEdge13: {
-  //   base: 'SauceLabs',
-  //   browserName: 'MicrosoftEdge',
-  //   version: '13',
-  //   platform: 'Windows 10'
-  // },
-  // slEdge14: {
-  //   base: 'SauceLabs',
-  //   browserName: 'MicrosoftEdge',
-  //   version: '14',
-  //   platform: 'Windows 10'
-  // },
-  // slEdge15: {
-  //   base: 'SauceLabs',
-  //   browserName: 'MicrosoftEdge',
-  //   version: '15',
-  //   platform: 'Windows 10'
-  // },
-  // slEdge: {
-  //   base: 'SauceLabs',
-  //   browserName: 'MicrosoftEdge',
-  //   platform: 'Windows 10'
-  // },
   slChrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome'
+    base: "SauceLabs",
+    browserName: "chrome",
   },
   slFirefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox'
-  }
+    base: "SauceLabs",
+    browserName: "firefox",
+  },
 };
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai'],
-    files: [
-      'dist/tests/browser.js',
-    ],
-    reporters: ['saucelabs'],
+    basePath: "",
+    frameworks: ["mocha", "chai"],
+    files: ["dist/tests/browser.js"],
+    reporters: ["saucelabs"],
     port: 9876,
     colors: true,
     singleRun: true,
     sauceLabs: {
-      build: 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')',
-      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      build:
+        "GH #" +
+        process.env.GITHUB_RUN_NUMBER +
+        " (" +
+        process.env.GITHUB_RUN_ID +
+        ")",
+      tunnelIdentifier: "github-action-tunnel",
       testName: `tastatur`,
       startConnect: false,
-      public: 'public'
+      public: "public",
     },
     browsers: Object.keys(browsers),
     customLaunchers: browsers,
@@ -77,4 +41,4 @@ module.exports = function(config) {
     browserDisconnectTolerance: 2,
     concurrency: 5,
   });
-}
+};
